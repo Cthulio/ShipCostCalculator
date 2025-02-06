0b89b1ae302be40c000a2ab82dbfbdca4bb2d33b
Nolan Crone <crone.nolan@gmail.com>
2025-02-05 20:50:09 -0500

    Init Commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    //class ShippingCalc
+    //  main()
+    //      vars
+    //      num itemPrice
+    //      num shippingCost
+    //      num costSummary
+    //      String keepGoing
+    //      do
+    //        Output “Please enter the price of your purchase:”
+    //        Input itemPrice
+    //        costSummary += itemPrice
+    //        Output “Do you have more? (Yes/No)” //* see below class
+    //        Input keepGoing
+    //      while keepGoing != “No”
+    //      site/source: using my own logic from Task 2 of Lab 2 from this week’s assignments. Removed/changed sets for certain variables as they don’t apply here.
+    //      if itemPrice>100 then
+    //        shippingCost = 0 //free!
+    //      else
+    //        shippingCost = costSummary*.02
+    //      end if
+    //      Output “Total price of the items: ” + costSummary
+    //      Output “Shipping Cost: ” + shippingCost
+    //      Output “Total: “ + (costSummary+shippingCost)
+    //  return
+    //end class
+    }
+}
