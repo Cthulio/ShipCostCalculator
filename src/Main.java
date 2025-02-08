828202c6166be0d022085c6cc729b5789d36eee2
Nolan Crone <crone.nolan@gmail.com>
2025-02-08 18:31:09 -0500

    finalized code, ran checks

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,35 @@
+import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-    //class ShippingCalc
-    //  main()
-    //      vars
-    //      num itemPrice
-    //      num shippingCost
-    //      num costSummary
-    //      String keepGoing
-    //      do
-    //        Output “Please enter the price of your purchase:”
-    //        Input itemPrice
-    //        costSummary += itemPrice
-    //        Output “Do you have more? (Yes/No)” //* see below class
-    //        Input keepGoing
-    //      while keepGoing != “No”
-    //      site/source: using my own logic from Task 2 of Lab 2 from this week’s assignments. Removed/changed sets for certain variables as they don’t apply here.
-    //      if itemPrice>100 then
-    //        shippingCost = 0 //free!
-    //      else
-    //        shippingCost = costSummary*.02
-    //      end if
-    //      Output “Total price of the items: ” + costSummary
-    //      Output “Shipping Cost: ” + shippingCost
-    //      Output “Total: “ + (costSummary+shippingCost)
-    //  return
-    //end class
+        //class ShippingCalc
+        //  main()
+        //      vars
+        //      num itemPrice
+        //      num shippingCost
+        //      site/source: using my own logic from Task 2 of Lab 2 from this week’s assignments. Removed/changed sets for certain variables as they don’t apply here.
+        //      if itemPrice>100 then
+        //        shippingCost = 0 //free!
+        //      else
+        //        shippingCost = costSummary*.02
+        //      end if
+        //      Output "Shipping Cost: "  + shippingCost
+        //      Output "Total: " + (costSummary+shippingCost)
+        //  return
+        //end class
+        Scanner in = new Scanner(System.in);
+        double itemPrice;
+        double shippingCost = -1;
+        System.out.println("Please input new item price:");
+        itemPrice = in.nextDouble();
+        in.nextLine();
+        if (itemPrice>=100){
+            shippingCost = 0; //free!
+        }else{
+            shippingCost = itemPrice * .02;
+        }
+        System.out.println("Shipping Cost: " + shippingCost);
+        System.out.println("Total: "+ (shippingCost+itemPrice));
     }
 }
